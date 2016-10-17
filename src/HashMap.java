20a5d62d29cafdcf00578d6da0500b83c5283999
ciborg245 <alejandrochgu@hotmail.com>
2016-10-17 14:19:37 -0600

    hashmap agregado

---
+++ src/HashMap.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 10/17/2016.
+ */
+/*
+public class HashMap<T> implements Map<T> {
+    public boolean insert (T element) {
+
+    }
+
+    public T find (T element) {
+
+    }
+}
+*/
