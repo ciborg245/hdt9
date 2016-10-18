c74cb7bbabe7f6797328966ee0d528de6eb7a3ea
Jose Jo Escobar <jo14343@uvg.edu.gt>
2016-10-17 19:10:35 -0600

    Agregando HashMap

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,14 +1,24 @@
 /**
  * Created by user on 10/17/2016.
  */
-/*
+
 public class HashMap<T> implements Map<T> {
+    public boolean valor;
     public boolean insert (T element) {
+        Map.put(objeto.getT(),objeto.getType());
 
     }
 
-    public T find (T element) {
-
+    public T find(T element) {
+        Map.containsValue(element.getT());
+        valor=Map.containsKey(element.getT());
+        if(valor==true){
+            T palabra=new T();
+            palabra.setT(element.getT());
+            palabra.setType(Map.get(element.getT()));
+            return palabra;
+        }else{
+            return null;
+        }
     }
 }
-*/
